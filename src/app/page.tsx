d88701a9b7545abc38cf1868d7a6d8798d263811
Herianto Tanoyo <herianto.tny@gmail.com>
2025-02-11 18:04:21 +0700

    Add profile card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,36 @@
 import Image from 'next/image'
 import chitChat from './img/chit-chat.svg'
+import erinLindford from './img/erin-lindford.90b9d461.jpg'
 
 export default function Home() {
   return (
-    // <h1 className="text-3xl font-bold underline">
-    //   Hello world!
-    // </h1>
-    <div className="mx-auto flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10 mt-10">
-      {/* <img className="size-12 shrink-0" src="/img/chit-chat.svg" alt="ChitChat Logo" /> */}
-      <Image
-        src={chitChat}
-        className="size-12 shrink-0"
-        alt="ChitChat Logo"
-      />
-      <div>
-        <div className="text-xl font-medium text-black dark:text-white">ChitChat</div>
-        <p className="text-gray-500 dark:text-gray-400">You have a new message!</p>
+    <div className='mx-auto flex flex-col gap-10'>
+
+      <div className="mx-auto flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10 mt-10">
+        <Image
+          src={chitChat}
+          className="size-12 shrink-0"
+          alt="ChitChat Logo"
+        />
+        <div>
+          <div className="text-xl font-medium text-black dark:text-white">ChitChat</div>
+          <p className="text-gray-500 dark:text-gray-400">You have a new message!</p>
+        </div>
       </div>
+
+      <div className="mx-auto flex max-w-sm flex-col gap-2 rounded-xl p-8 sm:flex-row sm:items-center sm:gap-6 sm:py-4 bg-white">
+        <Image className="mx-auto block h-24 w-24 rounded-full sm:mx-0 sm:shrink-0" src={erinLindford} alt="" />
+        <div className="space-y-2 text-center sm:text-left">
+          <div className="space-y-0.5">
+            <p className="text-lg font-semibold text-black">Erin Lindford</p>
+            <p className="font-medium text-gray-500">Product Engineer</p>
+          </div>
+          <button className="border-purple-200 text-purple-600 hover:border-transparent hover:bg-purple-600 hover:text-white active:bg-purple-700 ...">
+            Message
+          </button>
+        </div>
+      </div>
+
     </div>
   )
 }
