73a27b7ce58b0c97022c833290423dac793da469
heri2000 <heri.tny@gmail.com>
2025-02-27 21:29:55 +0700

    Move components into functions

--- src/app/page2/page.tsx
+++ src/app/page2/page.tsx
@@ -4,14 +4,20 @@ export default function Page2() {
   return(
     <div className="mx-auto flex flex-col w-6/12 gap-10 pb-20">
 
-      <div className="mx-auto mt-10">
-          Page <Link key="page2" href="/"><span className="bg-blue-900 p-2 rounded-full">1</span></Link> 2
-      </div>
+      {PageLinks()}
 
       <div>
           Page 2
       </div>
 
+      {PageLinks()}
+
     </div>
   )
 }
+
+const PageLinks = () => (
+  <div className="mx-auto mt-10">
+      Page <Link key="page2" href="/"><u>1</u></Link> 2
+  </div>
+)
